2121d2bb6e7fbd114539ada8fd7911b6cc7c8714
WilliamHaxhija <williamhaxhija@gmail.com>
2024-04-16 19:47:13 +0200

    init project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+    name: 'AppHeader'
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="container d-flex align-items-center py-3">
+            <img class="me-4" src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es" alt="logo">
+            <h1>Yu-Gi-Oh Api</h1>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+    img {
+        max-width: 10%;
+    }
+</style>
